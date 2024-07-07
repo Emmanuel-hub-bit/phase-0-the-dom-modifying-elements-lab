26e3cbb9c2eb3fea6ab008a33fd508868f2b040e
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-07-07 13:50:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// 1.
+document.querySelector("main").remove();
+
+// 2.
+const newHeader = document.createElement("h1");
+
+// 3.
+newHeader.id = "victory";
+
+// 4.
+newHeader.innerHTML = ("Emmanuel is the champion");
